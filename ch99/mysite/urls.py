93b7ebb6e43afc916c77720500bf3315b7e5b3e3
johnseo1026 <johnseo1026@hanmail.net>
2021-04-27 00:06:44 +0900

    blog

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -1,17 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from bookmark.views import BookmarkLV, BookmarkDV
+# from bookmark.views import BookmarkLV, BookmarkDV
 # from bookmark.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
 
     # class-based views
-    # path('bookmark/', include('bookmark.urls'))
-
-    path('bookmark/', BookmarkLV.as_view(), name = 'index'),
-    path('bookmark/<int:pk>/', BookmarkDV.as_view(), name = 'detail'),
+    # path('bookmark/', BookmarkLV.as_view(), name = 'index'),
+    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name = 'detail'),
 ]
