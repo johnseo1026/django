be80179cd79143db50793b80e1c4d9598d884622
johnseo1026 <johnseo1026@hanmail.net>
2021-04-29 00:10:41 +0900

    페이지

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 # from bookmark.views import BookmarkLV, BookmarkDV
 # from bookmark.views import *
@@ -7,6 +8,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
 
