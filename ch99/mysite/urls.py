e4010d057a4ec13e9514f0a5779f9b391bd27e32
johnseo1026 <johnseo1026@hanmail.net>
2021-05-05 18:17:26 +0900

    페이지

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -11,8 +11,4 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
-
-    # class-based views
-    # path('bookmark/', BookmarkLV.as_view(), name = 'index'),
-    # path('bookmark/<int:pk>/', BookmarkDV.as_view(), name = 'detail'),
 ]
