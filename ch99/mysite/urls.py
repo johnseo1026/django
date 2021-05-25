ea5e27439d9bac8817b4a9ef7222ff0543232ae7
johnseo1026 <johnseo1026@hanmail.net>
2021-05-25 22:20:41 +0900

    photo

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import HomeView
 
 # from bookmark.views import BookmarkLV, BookmarkDV
@@ -11,4 +13,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
-]
+    path('photo/', include('photo.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
---
+++ ch99/photo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.views.generic import ListView, DetailView
+
+from .models import Album, Photo
+
+app_name = 'photo'
+urlpatterns = [
+    # Example: /photo/
+    path('', ListView.as_view(model = Album), name = 'index'),
+
+    # Example: /photo/album/ (same as /photo/)
+    path('album/', ListView.as_view(model = Album), name='album_list'),
+
+    # Example: /photo/album/99/
+    path('album/<int:pk>/', DetailView.as_view(model = Album), name='album_detail'),
+
+    # Example: /photo/photo/99/
+    path('photo/<int:pk>/', DetailView.as_view(model = Photo), name='photo_detail'),
+    ]
