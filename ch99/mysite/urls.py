f6ff45edc6227a34fbe47666b0621e7ec678e066
johnseo1026 <johnseo1026@hanmail.net>
2021-05-29 16:46:21 +0900

    인증

--- ch99/mysite/urls.py
+++ ch99/mysite/urls.py
@@ -4,15 +4,18 @@ from django.contrib import admin
 from django.urls import path, include
 
 from .views import HomeView
-
+from .views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  # shkim
-                  path('', HomeView.as_view(), name='home'),
-                  path('bookmark/', include('bookmark.urls')),
-                  path('blog/', include('blog.urls')),
-                  path('photo/', include('photo.urls')),
+                path('admin/', admin.site.urls),
+                path('accounts/', include('django.contrib.auth.urls')),
+                path('accounts/register/', UserCreateView.as_view(), name='register'),
+                path('accounts/register/done/', UserCreateDoneTV.as_view(), name='register_done'),
+                # shkim
+                path('', HomeView.as_view(), name='home'),
+                path('bookmark/', include('bookmark.urls')),
+                path('blog/', include('blog.urls')),
+                path('photo/', include('photo.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
