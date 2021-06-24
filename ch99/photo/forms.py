39b1560e24061ba0c7754c0a21d80e3a285252d8
johnseo1026 <johnseo1026@hanmail.net>
2021-06-24 23:55:23 +0900

    콘텐츠편집

---
+++ ch99/photo/forms.py
@@ -0,0 +1,8 @@
+from django.forms import inlineformset_factory
+from .models import Album, Photo
+
+
+PhotoInlineFormSet = inlineformset_factory(Album, Photo,
+    fields = ['image', 'title', 'description'],
+    extra = 2)
+
